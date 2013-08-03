a24353ca86f4599aa6bcc3a4aa8d5f38d7240b11
Jon Hoffman <jonhoffman@gmail.com>
2013-08-03 12:30:23 -0400

    fixed white space

--- src/test/java/com/mongodb/FongoDBCollectionTest.java
+++ src/test/java/com/mongodb/FongoDBCollectionTest.java
@@ -8,52 +8,52 @@ import org.junit.Test;
 import com.foursquare.fongo.Fongo;
 
 public class FongoDBCollectionTest {
-	private FongoDBCollection collection;
-	
-	@Before
-	public void setUp() {
-		collection = (FongoDBCollection) new Fongo("test").getDB("test").getCollection("test");
-	}
-	
-	@Test
-	public void applyProjectionsInclusionsOnly() {
-		BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
-		DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("b", 1));
-		DBObject expected = new BasicDBObject().append("_id", "_id").append("b", "b");
-		
-		assertEquals("applied", expected, actual);
-	}
-	
-	@Test
-	public void applyProjectionsInclusionsWithoutId() {
-		BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
-		DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("_id", 0).append("b", 1));
-		BasicDBObject expected = new BasicDBObject().append("b", "b");
-		
-		assertEquals("applied", expected, actual);
-	}
-	
-	@Test
-	public void applyProjectionsExclusionsOnly() {
-		BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
-		DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("b", 0));
-		BasicDBObject expected = new BasicDBObject().append("_id", "_id").append("a", "a");
-		
-		assertEquals("applied", expected, actual);
-	}
-	
-	@Test
-	public void applyProjectionsExclusionsWithoutId() {
-		BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
-		DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("_id", 0).append("b", 0));
-		BasicDBObject expected = new BasicDBObject().append("a", "a");
-		
-		assertEquals("applied", expected, actual);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void applyProjectionsInclusionsAndExclusionsMixedThrowsException() {
-		BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
-		collection.applyProjections(obj, new BasicDBObject().append("a", 1).append("b", 0));
-	}
+  private FongoDBCollection collection;
+
+  @Before
+  public void setUp() {
+    collection = (FongoDBCollection) new Fongo("test").getDB("test").getCollection("test");
+  }
+
+  @Test
+  public void applyProjectionsInclusionsOnly() {
+    BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
+    DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("b", 1));
+    DBObject expected = new BasicDBObject().append("_id", "_id").append("b", "b");
+
+    assertEquals("applied", expected, actual);
+  }
+
+  @Test
+  public void applyProjectionsInclusionsWithoutId() {
+    BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
+    DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("_id", 0).append("b", 1));
+    BasicDBObject expected = new BasicDBObject().append("b", "b");
+
+    assertEquals("applied", expected, actual);
+  }
+
+  @Test
+  public void applyProjectionsExclusionsOnly() {
+    BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
+    DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("b", 0));
+    BasicDBObject expected = new BasicDBObject().append("_id", "_id").append("a", "a");
+
+    assertEquals("applied", expected, actual);
+  }
+
+  @Test
+  public void applyProjectionsExclusionsWithoutId() {
+    BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
+    DBObject actual = collection.applyProjections(obj, new BasicDBObject().append("_id", 0).append("b", 0));
+    BasicDBObject expected = new BasicDBObject().append("a", "a");
+
+    assertEquals("applied", expected, actual);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void applyProjectionsInclusionsAndExclusionsMixedThrowsException() {
+    BasicDBObject obj = new BasicDBObject().append("_id", "_id").append("a", "a").append("b", "b");
+    collection.applyProjections(obj, new BasicDBObject().append("a", 1).append("b", 0));
+  }
 }
